b8087115befed5c9c24be0daafbc04e8f6639e01
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-10-17 16:50:50 -0300

    add duas_strings e soma_variaveis

---
+++ aula10.17/duas_strings.py
@@ -0,0 +1,10 @@
+# você pode ESCAPAR aspas duplas utilizando aspas simples
+# vale reforçar que isso só vale para aspas duplas, outros caracteres não.
+# texto1 recebe o valor "sou do"
+texto1 = '"Sou do"'
+
+# texto2 recebe valor o "back-end"
+texto2 = "\"back-end\""
+
+# a função print, concatena e imprime as duas variaveis na tela
+print(texto1, texto2)
