4e7eb685d50194dbccf460076586f6b0ba1bbbdc
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-10-18 16:36:49 -0300

    Adicionado arquivo area_do_quadrado.py

---
+++ aula10.18/area_do_quadrado.py
@@ -0,0 +1,5 @@
+base = input('informe a base do quadrado: ')
+altura = input('Informe a altura do quadrado: ')
+calculo_da_area = int(base) * int(altura)
+
+print("A área do quadrado é:" , calculo_da_area)
