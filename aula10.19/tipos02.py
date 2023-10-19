fa8e8d7193e3078048516475b63944bbee58504d
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-10-19 15:34:12 -0300

    conteudo sobre tipos adicionado ao arquivo tipos02.py

--- aula10.19/tipos02.py
+++ aula10.19/tipos02.py
@@ -1,1 +1,11 @@
 # entender tipos e mudanças e tipos dentro do python
+
+a = 50
+b = "50"
+
+print(type(a))
+print(type(b))
+
+# utilização do float: moedas, peso, temperatura e outros casos parecidos
+peso = float(input("Qual o seu peso? "))
+print(type(peso))
