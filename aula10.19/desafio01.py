8ecf8bb329d80962629f46d28f6dfdff4acfea4b
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-10-22 17:27:28 -0300

    Adicionado arquivo ao aula10.19

---
+++ aula10.19/desafio01.py
@@ -0,0 +1,8 @@
+# Pedir a temperatura em graus Fahrenheit
+f = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+# Transformar a temperatura em graus Celsius usando a fórmula
+c = 5 * ((f-32) / 9)
+
+# Imprimir a temperatura em graus Celsius
+print("A temperatura em graus Celsius é: ", c)
