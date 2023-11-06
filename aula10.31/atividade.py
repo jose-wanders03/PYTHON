fb336a39a17d6408babf2f43cad1c2021363c9a1
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-11-06 14:12:39 -0300

    arquivos alterados

---
+++ aula10.31/atividade.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia a distância em Km e a quantidade de liros de combustível consumidos por um carro em um percurso qualquer. Calcule o consumo em Km/l e escreva uma mensagem de acordo com a relação abaixo:
+combus: float(input("Melhor combustivel:"))
+litr: float(input("Quantidade por litros:"))
+quil: float(input("Quantidade de quilometros:"))
+
+if combus / quil > litr == combus:
+    print("Qual o melhor:")
