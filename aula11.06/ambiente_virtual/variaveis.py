0b507514b0bdafc7a89df837685e5867e95d5e99
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-11-06 16:35:11 -0300

    Aula ambiente_virtual

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,16 @@
+# variaveis são usadas para salvar algo na memoria do computador.
+# O sinal de igual é utilizado para a atribição de valores
+var01 = "alguma coisa"
+print(var01)
+
+var01 = "outra coisa"
+print(var01)
+
+var01 = 10
+print(var01)
+varo2 = "A" + "B" "C"
+print(varo2) 
+varo2 = "A" * 10
+print(varo2)
+varo2 = 10 * "Paulo"
+print(varo2, "Paulo")
