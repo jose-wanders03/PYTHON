db028d24eb56a591c9a88a8a9e34558fe5ab8689
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-11-13 14:51:30 -0300

    Arquivos adicionados a aula11.09

---
+++ aula11.09/Atividade.py
@@ -0,0 +1,19 @@
+# Faça o fatiamento de 10 palavras do texto abaixo.
+
+# [i:f:p]
+
+Texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(Texto [0:5])
+print(Texto [5:9])
+print(Texto [9:17])
+print(Texto[20:30])
+print(Texto[33:44])
+print(Texto[16:20])
+print(Texto[49:53])
+print(Texto [60:67])
+print(Texto[76:88])
+print(Texto[66:74])
+
+
+
---
+++ aula11.09/Atividade02.py
@@ -0,0 +1,21 @@
+# Faça o fatiamento de 8 palavras e monte uma frase com a impressão dessas palavras.
+
+
+Texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(Texto[-80:-68])
+print(Texto[-68:-54])
+print(Texto[-54:-48])
+print(Texto[-48:-33])
+print(Texto[-33:-21])
+print(Texto[-21:-19])
+print(Texto[-19:-10])
+print(Texto[-10:-1])
+
+print( )
+
+
+
+
+
+
