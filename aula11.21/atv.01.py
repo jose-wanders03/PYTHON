0fa95869b625e55e068caad3c4d42b302faccfd4
jose Wanderson <josewandersons.coelho03@gmail.com>
2023-11-22 14:20:21 -0300

    arquivos for.py e atv.01.py adicionado a aula11.21

---
+++ aula11.21/atv.01.py
@@ -0,0 +1,10 @@
+# Crie um codigo que imprime todos os dias da semana na tela
+
+'''semana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
+for dia in semana:
+    print(dia)'''
+
+# escreva um codigo que imprima os numeros de 1 até 1000, e avise quando chegar nos multiplos de 100.
+for i in range(1,1001):
+    print(i)
---
+++ aula11.21/for.py
@@ -0,0 +1,32 @@
+# For trabalha com intervalos!!!
+# tem que possuir uma variavel de controle
+# iter()
+# next ()
+
+'''nome = 'Wanderson'
+letra = iter(nome)
+print(next(letra))
+print(next(letra))
+print(next(letra))
+print(next(letra))
+
+for letra in nome:
+    print(letra)'''
+
+# enumerate () é um interador de indices e valores
+
+lista_nomes = ['João', 'Pedro', 'Mateus', 'Judas', 'Tiago']
+
+lista_enumerada = enumerate(lista_nomes)
+print(lista_nomes)
+print(list(lista_enumerada))
+
+for item in lista_enumerada:
+    print(item)
+
+for item_lista in lista_enumerada:
+    print(item_lista)
+
+
+
+
